46ab3e5cca856d664b0a691d5100d63a96e1e3bd
AlexReyes03 <20233tn160@utez.edu.mx>
2025-02-12 17:50:20 -0600

    Subiendo productos

--- Proyecto-B/errorPages/errorPages/settings.py
+++ Proyecto-B/errorPages/errorPages/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app',
     'users',
+    'productos'
 ]
 
 MIDDLEWARE = [
---
+++ Proyecto-B/errorPages/productos/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from .models import Producto
+#Este Objeto me permite enviar respuesta JSON
+from django.http import JsonResponse
+
+from .forms import productoForm
+
+# Create your views here.
+
+
+#Vista que devuelve los productos como json
+def lista_productos(request):
+    #Obtener todos los objetos de productos de la bd
+    productos = Producto.objects.all()
+    
+    #Vamos a guardar los datos en un diccionario
+    #Este diccionario fue creado al aire y no es seguro
+    data = [
+        {
+            'nombre': p.nombre,
+            'precio': p.precio,
+            'imagen': p.imagen
+        }
+        for p in productos
+    ]
+    return JsonResponse(data, safe=False)
