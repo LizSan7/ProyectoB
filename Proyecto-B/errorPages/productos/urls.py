81ddd9ed4a9f1d5a37fcc6dfd41e962207f599b2
LizSan7 <20223tn031@utez.edu.mx>
2025-02-12 18:56:40 -0600

    Subiendo formulario

--- Proyecto-B/errorPages/productos/urls.py
+++ Proyecto-B/errorPages/productos/urls.py
@@ -3,5 +3,6 @@ from . views import *
 
 urlpatterns= [
     path('api/get', lista_productos, name='lista'),
-    path('ver/', ver_productos, name='ver')
+    path('ver/', ver_productos, name='ver'),
+    path('agregar/', agregar_producto, name= 'agregar')
 ]
--- Proyecto-B/errorPages/productos/views.py
+++ Proyecto-B/errorPages/productos/views.py
@@ -3,10 +3,25 @@ from .models import Producto
 #Este Objeto me permite enviar respuesta JSON
 from django.http import JsonResponse
 
-# from .forms import productoForm
-#ya
+from .forms import productoForm
+
 # Create your views here.
 
+#Una vista que cargue y maneje al mismo tiempo el formulario
+def agregar_producto(request):
+    #checar si vengo del formulario
+    if request.method == 'POST':
+        #Quiere decir que enviaron el form
+        form = productoForm(request.POST)
+        #Checar que sus datos estén bien
+        if form.is_valid():
+            #lo guardo en bd
+            form.save()
+            return redirect('ver')
+        #Que pasa si no fue que mandaron el form
+    else:
+            form = productoForm
+    return render(request, 'agregar.html',{'form': form})
 
 #Vista que devuelve los productos como json
 def lista_productos(request):
