c7b2afdd10310d4fbe96262008fd9be2e4e9ccfb
LizSan7 <20223tn031>
2025-02-19 17:34:39 -0600

    Subiendo registro de producto con un objeto JSON

--- Proyecto-B/errorPages/productos/views.py
+++ Proyecto-B/errorPages/productos/views.py
@@ -42,3 +42,32 @@ def lista_productos(request):
 
 def ver_productos(request):
     return render(request, 'ver.html')
+
+import json
+#Función que agrega un producto con un objeto JSON
+def registrar_producto(request):
+    #Checa si muestra request es de tipo POST
+    if request.method == 'POST':
+        #quiere decir que si estoy manejando el request
+        try:
+            data = json.loads(request.body) #Parametro es un texto que debería ser un JSON
+            producto = Producto.objects.create(
+                nombre = data['nombre'],
+                precio = data['precio'],
+                imagen = data['imagen']
+            ) #Create directamente mete el objeto en la BD
+            return JsonResponse(
+                {
+                    'mensaje': 'Registro exitoso',
+                    'id': producto.id    
+                }, status=201
+            )
+        except Exception as e:
+            print(str(e))
+            return JsonResponse(
+                {'error': str(e)}, status = 400
+            )
+    #Si no es POST el request...
+    return JsonResponse (
+        {'error': 'El método no es soportado'}, status = 405
+    )
