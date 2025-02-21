8692eced2fb8a25f9f8d57e6a2cc07109eb9ba86
LizSan7 <20223tn031@utez.edu.mx>
2025-02-20 19:34:03 -0600

    Subiendo funcion urls producto

--- Proyecto-B/errorPages/productos/urls.py
+++ Proyecto-B/errorPages/productos/urls.py
@@ -7,4 +7,6 @@ urlpatterns= [
     path('agregar/', agregar_producto, name= 'agregar'),
     path('api/post/', registrar_producto, name='post'),
     path('api/put/<str:id_producto>/', actualizar_producto, name='put'),
+    path('api/delete/<str:id_producto>/', borrar_producto, name='delete'),
+    path('api/get/<str:id_producto>/', obtener_producto, name='get'),
 ]
